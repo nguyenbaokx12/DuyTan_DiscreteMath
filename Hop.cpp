840e4dbbb932f977bce53f66f02a38e4b56c0923
MacBook Air <macbookair@MacBooks-MacBook-Air.local>
2023-03-01 16:35:34 +0700

    up

---
+++ Hop.cpp
@@ -0,0 +1,75 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+//------------------HOP----------------//
+int A[10],n,B[10],m,C[10],k;
+//------------------
+
+int Init(int M[], int &k, char *path){
+	FILE *fptr;
+	if((fptr = fopen(path,"r")) == NULL){
+		system("Error");
+		return 1;
+	}
+	k = 0;
+	while(!feof(fptr)){
+		fscanf(fptr,"%d", &M[k]);
+		k++;
+	}
+	fclose(fptr);
+	return 0;
+}
+//-----------------
+
+void Delete(int M[], int &n){
+	int i,j,k;
+	for(i = 0; i < n-1; i++)
+	for(j = i+1; j < n; j++)
+		if(M[i] == M[j]){
+			for(k = j; k < n-1; k++)
+				M[k] = M[k+1];
+				n = n-1;
+				j = j-1;
+		}
+}
+//-----------------
+
+void Print(int M[], int n){
+	for(int i =0; i < n; i++)
+	printf("%d\t",M[i]);
+	printf("\n");
+}
+//------------------
+
+void union_set(int A[], int n, int B[], int m, int C[], int &k){
+	int i,j;
+	k=0;
+	for(i = 0; i < n; i++){
+		C[i] = A[i];
+		k = k+1;
+	}
+	i = 0;
+	while(i < m) {
+		int ok =1;
+		for(j = 0; j < k; j++)
+			if(C[j] == B[i]){
+				ok = 0;
+				break;
+			}
+		if(ok == 1){
+			C[k] = B[i];
+			k = k+1;
+		}
+		i++;
+	}
+}
+//--------------------
+
+int main(int argc, char** argv) {
+	if(Init(A,n,(char*)"Set_A.INP") == 0&&Init(B,m,(char*)"Set_B.INP")==0){
+		printf("A:\t"); Print(A,n);
+		printf("\nB:\t");  Print(B,m);
+		Delete(A,n); Delete(B,m);
+//		printf("\nC:\t"); Print(C,k);
+	}
+}
